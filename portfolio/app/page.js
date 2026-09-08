3c32d7229eb75b3ab6deef884d467301f50dec5a
lAmanSinghl <120amansingh@gmail.com>
2026-09-08 16:17:34 +0530

    2nd

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -362,7 +362,7 @@ export default function Home() {
         <div className="sticky top-0 h-screen overflow-hidden">
 
           <motion.div style={{ x: useTransform(horizontalProgress, [0, 0.75], ["0%", "-170%"]) }} className="flex">
-            <section className="w-screen h-screen shrink-0">
+            <section className="w-screen h-screen shrink-0 relative top-2">
 
               <div className=" absolute top-18 left-330  w-70 h-78  overflow-hidden rounded-xl scale-90">
                 <img src="IMG_20260101_111852.jpg.jpeg" alt="" className="absolute w-200 scale-200 -top-35 right-10 " />
@@ -371,15 +371,15 @@ export default function Home() {
                 <HorizontalReveal progress={horizontalProgress} color="#D5F831" trigger={0.15}><div className=" relative -left-4 text-2xl scale-y-55 scale-x-70 ">NOIDA, 2024</div></HorizontalReveal>
               </div>
 
-              <div className=" absolute top-94 left-380  w-70 h-78  overflow-hidden rounded-xl scale-78"><img src="Screenshot_2026-06-11-12-08-43-003_com.instagram.android-edit.jpg.jpeg" style={{ filter: "grayscale(150%) sepia(50%) brightness(0.6) contrast(0.95)" }} alt="" className="absolute w-200 scale-130 -top-25 right-10 opacity-80" /></div>
+              <div className=" absolute top-103 left-380  w-70 h-78  overflow-hidden rounded-xl scale-78"><img src="Screenshot_2026-06-11-12-08-43-003_com.instagram.android-edit.jpg.jpeg" style={{ filter: "grayscale(150%) sepia(50%) brightness(0.6) contrast(0.95)" }} alt="" className="absolute w-200 scale-130 -top-25 right-10 opacity-80" /></div>
 
-              <div className="absolute text-[#B4B8A5] left-412 top-162 w-35 scale-70">
+              <div className="absolute text-[#B4B8A5] left-412 top-171 w-35 scale-70">
                 <HorizontalReveal progress={horizontalProgress} color="#D5F831" trigger={0.15}><div className=" relative left-5 text-2xl scale-y-55 scale-x-70 ">JAIPUR, 2018</div></HorizontalReveal>
               </div>
 
 
               <div className="relative top-70 left-430 w-fit scale-70 font-datatype text-[#B4B8A5] ">
-                <img src="school1.webp" alt="" className="rounded-xl w-220" />
+                <img src="952101786200610_School_Photo_2026.jpg" alt="" className="rounded-xl w-222 relative " />
                 <div className="absolute bottom-123 -left-1 w-80 ">
                   <HorizontalReveal progress={horizontalProgress} color="#D5F831" trigger={0.15}><div className=" relative -left-10 text-2xl scale-y-55 scale-x-70 ">MUGHALSARAI, 2008-2022</div></HorizontalReveal>
                 </div>
@@ -402,13 +402,13 @@ export default function Home() {
               <div className="absolute text-[#4a4b48]  left-635 top-82 w-42 scale-70">
                 <HorizontalReveal progress={horizontalProgress} color="#D5F831" trigger={0.45}><div className=" relative -left-4 text-2xl scale-y-55 scale-x-70 ">PUSHKAR, 2024</div></HorizontalReveal>
               </div>
-              <div className=" absolute top-90 left-680  w-60 h-78  overflow-hidden rounded-xl "><img src="IMG-20250428-WA0003.jpg.jpeg" alt="" className="absolute w-200 scale-160 -top-18 " /></div>
-              <div className="absolute text-[#4a4b48]  left-717 top-167 w-42 scale-70">
+              <div className=" absolute top-92 left-680  w-60 h-78  overflow-hidden rounded-xl "><img src="SAVE_20260908_154138.jpg.jpeg" alt="" className="absolute w-200 scale-160 -top-18 " /></div>
+              <div className="absolute text-[#4a4b48]  left-717 top-169 w-42 scale-70">
                 <HorizontalReveal progress={horizontalProgress} color="#D5F831" trigger={0.45}><div className=" relative -left-4 text-2xl scale-y-55 scale-x-70 ">Delhi, 2025</div></HorizontalReveal>
               </div>
 
               <div className="absolute top-73  left-735 w-210  scale-80 font-datatype text-[#565752] ">
-                <img src="a7.jpg (1).jpeg" alt="" className="rounded-xl w-220" />
+                <img src="a7.jpg.jpeg" alt="" className="rounded-xl w-220" />
                 <div className="absolute bottom-110 -left-1 w-80 scale-y-70">
                   <HorizontalReveal progress={horizontalProgress} color="#D5F831" trigger={0.51}><div className=" relative -left-6 text-2xl scale-y-55 scale-x-70 text-[#4a4b48]">NOIDA, 20023-2027</div></HorizontalReveal>
                 </div>
